58e814b9b505452e3efdd47952788a5fb9971da4
Alfredo Delgado <alfred@adelgado.org>
2017-04-24 11:51:08 -0400

    add continuous integration

--- src/test/functional.js
+++ src/test/functional.js
@@ -1,14 +1,6 @@
 import F from 'funcunit';
 import QUnit from 'steal-qunit';
 
-import 'nyandc/models/test';
-
-import 'nyandc/home-page/home-page-test';
-
-import 'nyandc/nav-bar/nav-bar-test';
-
-import 'nyandc/product-page/product-page-test';
-
 F.attach(QUnit);
 
 QUnit.module('nyandc functional smoke test', {
---
+++ src/test/test.js
@@ -0,0 +1,12 @@
+import QUnit from 'steal-qunit';
+
+//import 'nyandc/test/functional';
+
+import 'nyandc/models/test';
+
+import 'nyandc/home-page/home-page-test';
+
+import 'nyandc/nav-bar/nav-bar-test';
+
+import 'nyandc/product-page/product-page-test';
+
