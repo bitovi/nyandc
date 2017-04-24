44f5020e0e647c1df8560a308f37273a48b746dd
Alfredo Delgado <alfred@adelgado.org>
2017-04-24 18:59:07 -0400

    Find and the display the closest store.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import DefineMap from 'can-define/map/';
 import route from 'can-route';
 import 'can-route-pushstate';
+import Store from '~/models/store';
 
 route('{page}', { page: 'home' });
 route('{page}/{productId}');
@@ -26,6 +27,44 @@ const AppViewModel = DefineMap.extend({
   title: {
     value: 'NY&C',
     serialize: false
+  },
+  /**
+   * @property {Boolean} hasGeolocation
+   *
+   * Whether the client has the geolocation api.
+   */
+  get hasGeolocation() {
+    return typeof navigator !== 'undefined' && 'geolocation' in navigator;
+  },
+  /**
+   * @property {Store} closestStore
+   *
+   * The store closest to the user.
+   */
+  closestStore: {
+    serialize: false,
+    value: undefined
+  },
+  init() {
+    if (window) {
+      window.appState = this;
+    }
+
+    if (this.hasGeolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        Store.findAll({ lon, lat })
+          .then(stores => {
+            this.closestStore = stores[0];
+
+            return stores;
+          })
+          .catch(error => console.error(error));
+      },
+      error => console.error(error));
+    }
   }
 });
 
---
+++ src/models/store.js
@@ -0,0 +1,50 @@
+import DefineMap from 'can-define/map/';
+import DefineList from 'can-define/list/';
+import superMap from 'can-connect/can/super-map/';
+import feathers from '~/models/feathers';
+
+export const Store = DefineMap.extend({
+  _id: 'string',
+  zoomLevel: 'number',
+  dayHour7: 'string',
+  dayHour6: 'string',
+  dayHour5: 'string',
+  dayHour4: 'string',
+  dayHour3: 'string',
+  dayHour2: 'string',
+  dayHour1: 'string',
+  latitude: 'number',
+  longitude: 'number',
+  location: { },
+  postalCode: 'string',
+  City: 'string',
+  Mile: 'number',
+  StreetAddress: 'string',
+  StoreID: 'number',
+  State: 'string',
+  PhoneNumber: 'string',
+  suburb: 'string',
+  storeName: 'string',
+  createdAt: Date,
+  updatedAt: Date
+});
+
+Store.List = DefineList.extend({
+  '*': Store
+});
+
+export const messageConnection = superMap({
+  url: feathers.socketio('stores'),
+  idProp: '_id',
+  Map: Store,
+  List: Store.List,
+  name: 'store'
+});
+
+// Connect to realtime events.
+feathers.io.on('stores created', store => store.createInstance(store));
+feathers.io.on('stores updated', store => store.updateInstance(store));
+feathers.io.on('stores patched', store => store.updateInstance(store));
+feathers.io.on('stores removed', store => store.destroyInstance(store));
+
+export default Store;
